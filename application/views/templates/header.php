7103da28eaebbfedc97e9411e350df06aede0d46
Yoan <yoan.rab@gmail.com>
2023-09-29 00:18:05 +0300

    dynamisation login, accueil, besoins

---
+++ application/views/templates/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="<?= base_url('assets\css\navbar.css') ?>">
+	<link rel="stylesheet" href="<?= base_url('assets\css\creation.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets\css\home.css') ?>">
+	<script src="<?=base_url('assets/js/navbar.js') ?>" defer></script>
+	<title>Definition de besion</title>
+</head>
+<body>
