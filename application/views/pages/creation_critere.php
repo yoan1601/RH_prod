0ca4b37fd26dff76e2566f87e9360901d6697731
JossElito179 <josselito.303@gmail.com>
2023-09-29 10:14:47 +0300

    register critere done with front-end

--- application/views/pages/creation_critere.php
+++ application/views/pages/creation_critere.php
@@ -1,16 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
+<?php 
+	$this->load->view('templates/header');
+	$this->load->view('templates/navbar');
+?>
+	<div class="critere_div">
     <button id="ajoutCritere" onclick="ajoutCritere()">Ajouter critere</button>
-    <div>
-        <form action="<?= site_url('critere/setInSession') ?>" method="post">
+    <div class="creation_form">
+	<p><label for="">Creer vos critere de Selection</label></p>
+        <form  action="<?= site_url('critere/setInSession') ?>" method="post">
             <input type="hidden" id="nbCriteres" name="nbCriteres" value="0">
             <input type="hidden" id="nbOptions" name="nbOptions" value="0">
             <ul id="criteres">
@@ -20,6 +16,7 @@
         </form>
         <button type="button" id="ajoutOption" onclick="ajoutOption()" style="display: none;">Ajouter option</button>
     </div>
+	</div>
     <script src="<?= base_url('assets/js/criteres.js') ?>"></script>
 </body>
 
