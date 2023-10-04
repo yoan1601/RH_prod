27b745c0f2873d2113f366bbb9d93046fc414a76
JossElito179 <josselito.303@gmail.com>
2023-10-04 16:10:11 +0300

    accueil-front fini

---
+++ application/views/pages/front-office/home.php
@@ -0,0 +1,84 @@
+<?php 
+	$this->load->view('templates/navbarFront.php');
+?>
+
+	<div class="content-build">
+	<center>
+		<div class="container-welcome">
+		
+			<p><h2>Welcome in Dev Soft</h2></p>
+				<p>
+					<div class="input-container-home">
+						<p>Filtre des annonces
+						<select name="" id="">
+							<option value="">Tous</option>
+							<option value="">Finance</option>
+						</select>
+						</p>
+					</div>
+				</p>
+		</div>
+	</center>
+		
+		<center>
+			<div class="container-photo"></div>		
+		</center>
+	</div>
+
+	<div class="card">
+			<div class="card-body">
+				<p></p>
+				<p></p>
+				<p class="fin">nun</p>
+				<p><h3 class="card-title">Liste des annonces</h3></p>
+              <div class="list-group">
+				<table>
+					<tr class="title_text">
+						<td>Debut d'annonce</td>
+						<td>Services</td>
+						<td>Date</td>
+						<td>Action</td>
+					</tr>
+					<tr>
+						<td>Lorem ipsum dolor sit, amet consectetur adipisicing elit...</td>
+						<td>Finance</td>
+						<td>12/09/2023</td>
+						<td><button>Voir plus</button></td>
+					</tr>
+					<tr>
+						<td>Lorem ipsum dolor sit, amet consectetur adipisicing elit...</td>
+						<td>Finance</td>
+						<td>12/09/2023</td>
+						<td><button>Voir plus</button></td>
+					</tr>
+					<tr>
+						<td>Lorem ipsum dolor sit, amet consectetur adipisicing elit...</td>
+						<td>Finance</td>
+						<td>12/09/2023</td>
+						<td><button>Voir plus</button></td>
+					</tr>
+					<tr>
+						<td>Lorem ipsum dolor sit, amet consectetur adipisicing elit...</td>
+						<td>Finance</td>
+						<td>12/09/2023</td>
+						<td><button>Voir plus</button></td>
+					</tr>
+					<tr>
+						<td>Lorem ipsum dolor sit, amet consectetur adipisicing elit...</td>
+						<td>Finance</td>
+						<td>12/09/2023</td>
+						<td><button>Voir plus</button></td>
+					</tr>
+					<tr>
+						<td>Lorem ipsum dolor sit, amet consectetur adipisicing elit...</td>
+						<td>Finance</td>
+						<td>12/09/2023</td>
+						<td><button>Voir plus</button></td>
+					</tr>
+				</table>
+              </div>
+            </div>
+        </div>
+
+</body>
+</html>
