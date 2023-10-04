e309341ae18207bd4f2c6b69b8d75c133be66ca5
JossElito179 <josselito.303@gmail.com>
2023-10-04 08:42:51 +0300

    listes des tests fini

--- application/controllers/Front.php
+++ application/controllers/Front.php
@@ -59,6 +59,6 @@ class Front extends CI_Controller {
 	public function linker(){
 		$data = [];
         $data['services'] = $this->service->getAllServices();
-		$this->load->view('pages/resumeEntretien',$data);
+		$this->load->view('pages/listeTest',$data);
 	}
 }
