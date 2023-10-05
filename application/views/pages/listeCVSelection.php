66dac100414909c22575062ad17cb34eb5aa1e07
Yoan <yoan.rab@gmail.com>
2023-10-05 20:57:43 +0300

    integration tests

--- application/views/pages/listeCVSelection.php
+++ application/views/pages/listeCVSelection.php
@@ -1,35 +1,51 @@
-<?php 
-	$this->load->view('templates/header');
-	$this->load->view('templates/navbar');
+<?php
+$this->load->view('templates/header');
+$this->load->view('templates/navbar');
 ?>
 
 <div class="main-list">
-<div class="card">
-			<div class="card-body">
-				<p></p>
-				<p></p>
-				<p><h3 class="card-title">Marketing</h3></p>
-              <div class="list-group">
-				<table>
-					<tr class="title_text">
-						<td></td>
-						<td>Postulant</td>
-						<td>Date de reception</td>
-						<td>Note</td>
-						<td>Action</td>
-					</tr>
-					<tr>
-						<td><input type="checkbox" name="CVselected" id=""></td>
-						<td>Rakotoson Benja</td>
-						<td>12/09/2023</td>
-						<td>2 jours</td>
-						<td><button>Details</button></a></td>
-					</tr>
-				</table>
-              </div>
-            </div>
-        </div>
+	<div class="card">
+		<div class="card-body">
+			<p></p>
+			<p></p>
+			<p>
+			<h3 class="card-title"><?= $service->nom_service ?></h3>
+			</p>
+			<form class="w-100" action="<?= site_url('test/insertCvSelection') ?>" method="post">
+				<input type="hidden" name="idService" value="<?= $idService ?>">
+				<div class="list-group">
+					<table>
+						<tr class="title_text">
+							<td></td>
+							<td>Postulant</td>
+							<td>Date de reception</td>
+							<td>Duree</td>
+							<td>Note</td>
+							<td>Action</td>
+						</tr>
+						<?php if ($liste_cv !== false) { ?>
+							<?php foreach ($liste_cv as $cv) { ?>
+								<tr>
+									<td><input type="checkbox" name="cv[]" value="<?= $cv->id_cv ?>"> <input type="hidden" name="idRecrutement" value="<?= $cv->id_recrutement ?>"></td>
+									<td><?= $cv->nom ?> <?= $cv->prenom ?></td>
+									<td><?= $cv->reception ?></td>
+									<td><?= $cv->duree ?> jrs</td>
+									<td><?= $cv->note ?></td>
+									<td><button>Details</button></a></td>
+								</tr>
+							<?php } ?>
+						<?php } else {
+							echo 'donnée vide';
+						} ?>
+					</table>
+				</div>
+		</div>
+		<?php if ($liste_cv !== false) { ?>
+			<button type="submit">Selectionner et planifier le test</button>
+		<?php } ?>
+		</form>
+	</div>
 </div>
 </body>
-</html>
 
+</html>
--- application/views/pages/questionnaire1.php
+++ application/views/pages/questionnaire1.php
@@ -1,42 +1,51 @@
-<?php 
-	$this->load->view('templates/header');
-	$this->load->view('templates/navbar');
+<?php
+$this->load->view('templates/header');
+$this->load->view('templates/navbar');
 ?>
 
 
 
 <div class="main-quest1">
-	<form action="" method="post">
-	<p class="fin">nun</p>
-	<p><center><h3>Questionnaire</h3></p></center>
-	<div class="form-div">
-	<p>Question 1<input type="number" placeholder="Note" name="" id=""></p>
-	<p>
-		<ul class="rep">
-			<li class="ans"><input type="checkbox" name="" id=""> Ans 1</li>
-			<li class="ans"><input type="checkbox" name="" id=""> Ans 2</li>
-			<li class="ans"><input type="checkbox" name="" id=""> Ans 3</li>
-		</ul>
-	</p>
-	<p>Question 1<input type="number" placeholder="Note" name="" id=""></p>
-	<p>
-		<ul class="rep">
-			<li class="ans"><input type="checkbox" name="" id=""> Ans 1</li>
-			<li class="ans"><input type="checkbox" name="" id=""> Ans 2</li>
-			<li class="ans"><input type="checkbox" name="" id=""> Ans 3</li>
-		</ul>
-	</p>
-	<p>Question 1<input type="number" placeholder="Note" name="" id=""></p>
-	<p>
-		<ul class="rep">
-			<li class="ans"><input type="checkbox" name="" id=""> Ans 1</li>
-			<li class="ans"><input type="checkbox" name="" id=""> Ans 2</li>
-			<li class="ans"><input type="checkbox" name="" id=""> Ans 3</li>
-		</ul>
-	</p>
-	</div>
-	<button>Generer le questionnaire</button>
-	<p class="fin">nun</p>
+	<button id="ajoutquestion" onclick="ajoutQuestion()">Ajouter une question</button>
+	<form action="<?= site_url('test/save') ?>" method="post">
+		<input type="hidden" id="nbQuestions" name="nbQuestions" value="0" placeholder="question">
+		<input type="hidden" id="nbReponses" name="nbReponses" value="0">
+		<input type="hidden" id="idService" name="idService" value="<?= $idService ?>">
+		<p class="fin">nun</p>
+		<p>
+			<center>
+				<h3>Questionnaire</h3>
+		</p>
+		</center>
+		<div class="form-div" id="questions">
+				<!-- <p><input type="text" name="" id=""><input type="number" placeholder="Note" name="" id=""></p>
+				<p>
+				<ul class="rep">
+					<li class="ans"><input type="checkbox" name="" id=""> <input type="text" name="" id=""></li>
+					<li class="ans"><input type="checkbox" name="" id=""> Ans 2</li>
+					<li class="ans"><input type="checkbox" name="" id=""> Ans 3</li>
+				</ul>
+				</p>
+				<p>Question 1<input type="number" placeholder="Note" name="" id=""></p>
+				<p>
+				<ul class="rep">
+					<li class="ans"><input type="checkbox" name="" id=""> Ans 1</li>
+					<li class="ans"><input type="checkbox" name="" id=""> Ans 2</li>
+					<li class="ans"><input type="checkbox" name="" id=""> Ans 3</li>
+				</ul>
+				</p>
+				<p>Question 1<input type="number" placeholder="Note" name="" id=""></p>
+				<p>
+				<ul class="rep">
+					<li class="ans"><input type="checkbox" name="" id=""> Ans 1</li>
+					<li class="ans"><input type="checkbox" name="" id=""> Ans 2</li>
+					<li class="ans"><input type="checkbox" name="" id=""> Ans 3</li>
+				</ul>
+				</p> -->
+		</div>
+		<button id="soumission" type="submit">Generer le questionnaire</button>
+		<p class="fin">nun</p>
 	</form>
-
+	<button type="button" id="ajoutReponse" onclick="ajoutReponse()" style="display: none;">Ajouter une reponse</button>
 </div>
+<script src="<?= base_url('assets/js/questionnaire.js') ?>"></script>
