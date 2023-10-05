76509f16c4466f3dbfffcbb0f9c4a14a62f7e526
JossElito179 <josselito.303@gmail.com>
2023-10-05 10:03:15 +0300

    detailNotification fini

---
+++ application/views/pages/front-office/detailNotification.php
@@ -0,0 +1,14 @@
+<?php 
+	$this->load->view('templates/navbarFront');
+?>
+
+<center><h2>Details notification</h2></center>
+<div class="congrates">
+	<p class="trans">...</p>
+		<p><h3>Recrutement: Marketing</h3></p>
+		<p>Chere <label for="">Andry Niriana Rajoelina </label></p>
+		<p>Vous etes admis a l'entretien de marketing qui se tiendra le 1/1/2222 08:00 a Tana</p>
+		<p class="trans">...</p>
+	</div>
+</body>
+</html>
