53d0093fc0b715e4028fa31b45b8cff371f9ed9a
Yoan <yoan.rab@gmail.com>
2023-10-06 06:08:55 +0300

    acceuil liste annonce all

--- application/models/ServiceModel.php
+++ application/models/ServiceModel.php
@@ -20,19 +20,25 @@ class ServiceModel extends CI_Model {
 	 */
 
 	public function getAllServices() {
+        $user = $this->session->user;
+        
+        // departement 
+        $this->db->where('id_dept ', $user->id_dept);
+
         // Utilisez la fonction where() pour spécifier la condition WHERE
         $this->db->where('etat_service >', 0);
 
         // Effectuez la requête SELECT sur la table "services"
-        $query = $this->db->get('services');
+        $query = $this->db->get('v_service_dept');
 
         // Retournez les résultats de la requête
         return $query->result();
     }
 
     public function getServiceById($idService){
-        $query="select * from services where id_service=%s";
-        $query=sprintf($query, $idService);
+        $user = $this->session->user;
+        $query="select * from v_service_dept where id_service=%s and id_dept=%s";
+        $query=sprintf($query, $idService, $user->id_dept);
         $query=$this->db->query($query);
         $query=$query->result();
         if(count($query)>0){
