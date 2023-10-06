de028df3178764c052f3aa38c8ff0e1131543838
JossElito179 <josselito.303@gmail.com>
2023-10-06 04:49:45 +0300

    en minute dans plannification entretien

--- application/views/pages/planificationEntretien.php
+++ application/views/pages/planificationEntretien.php
@@ -14,8 +14,8 @@
 			<p><input type="datetime-local" name="" id=""></p>
 		<p class="sentence">Lieu du test</p>
 			<p><input type="text" name="" id=""></p>
-		<p class="sentence">Durre de l'entretien pour un candidat</p>
-			<p><input type="time" name="" id=""></p>
+		<p class="sentence">Durre de l'entretien pour un candidat en minute</p>
+			<p><input type="number" name="" id=""></p>
 			<p>Nombre de candidats:<label for="">5</label></p>
 		<p><center><button>Planifier l'entretien</button></center></p>
 		<p class="fin">nun</p>
