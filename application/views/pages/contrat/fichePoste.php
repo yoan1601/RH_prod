82509f9d14e0538100c52e41be4bf931a160f56e
JossElito179 <118587500+JossElito179@users.noreply.github.com>
2023-10-12 21:50:10 +0300

    jusqu'a generation fiche de poste

---
+++ application/views/pages/contrat/fichePoste.php
@@ -0,0 +1,43 @@
+<?php 
+	$this->load->view('templates/header');
+	$this->load->view('templates/navbar');
+?>
+    <p><center><h2>Fiche de poste</h2></center></p>
+    <div class="container-contratResume">
+    <div class="firstPart">
+    <p><center><h3>Numero matricule</h3></center></p>
+        <p><h4>Informations personnelles de l'employee</h4></p>
+        <p>Nom:Rakoto</p>
+        <p>Prenom:Be</p>
+        <p>Genre:Homme</p>
+        <p>Contact:+261 38 901 90</p>
+        <p>Adresse:Lot NeinNeinNein</p>
+        <p><h4>Informations sur la fonction</h4></p>
+        <p>Poste:</p>
+        <p>Mission:</p>
+        <p>Categorie:</p>
+        <p>Date de changement du contrat: 90</p>
+        <p>Affiliation CNAPS:OUI</p>
+        <p>Affiliation Organisme sanitaire:OUI</p>
+    </div>
+    <div class="secondPart">
+        <p><h4>Listes des avantages</h4></p>
+        <p>Iphone 15 pro max</p>
+        <p>Lamborghini Urus V10</p>
+        <p>Villa au bord de la plage</p>
+        <p><h4>Superieur hierarchique</h4></p>
+        <p><input type="checkbox" name="" id=""> IT manager</p>
+        <p><input type="checkbox" name="" id=""> IT manager</p>
+        <p><input type="checkbox" name="" id=""> IT manager</p>
+        <p><h4>Subordonnes </h4></p>
+        <p><input type="checkbox" name="" id=""> IT dev</p>
+        <p><input type="checkbox" name="" id=""> IT test</p>
+        <p><input type="checkbox" name="" id=""> IT QA</p>
+        <p class="trans">nunn</p>
+    </div>
+
+    </div>
+        <p class="trans">nunn</p>
+    <center><button>Genere le fiche de poste</button></center>
+</body>
+</html>
