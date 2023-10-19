6fbaee0cffbc6df59da73dd9dc4503935b57526d
JossElito179 <118587500+JossElito179@users.noreply.github.com>
2023-10-19 21:16:17 +0300

    liste demande de conge faite

---
+++ application/views/pages/employesConge/ListeCongeDemande.php
@@ -0,0 +1,45 @@
+<?php 
+	$this->load->view('templates/header');
+	$this->load->view('templates/navbar');
+?>
+	  	<div class="card list">
+			<div class="card-body">
+				<p></p>
+				<p></p>
+				<p class="fin">nun</p>
+				<p><h3 class="card-title">Demandes de conge en attente</h3></p>
+              <div class="list-group">
+				<table>
+					<tr class="title_text">
+						<td>Numero Matricule</td>
+						<td>Nom prenom</td>
+						<td class="listeAdmis">Actions</td>
+					</tr>
+					<tr>
+                        <td>EMP0067</td>
+                        <td>Rakoto Doe</td>
+						<td class="listeAdmis"><button class="embauche">Detail</button></td>
+					</tr>
+                    <tr>
+                        <td>EMP0067</td>
+                        <td>Rakoto Doe</td>
+						<td class="listeAdmis"><button class="embauche">Detail</button></td>
+					</tr>
+                    <tr>
+                        <td>EMP0067</td>
+                        <td>Rakoto Doe</td>
+						<td class="listeAdmis"><button class="embauche">Detail</button></td>
+					</tr>
+                    <tr>
+                        <td>EMP0067</td>
+                        <td>Rakoto Doe</td>
+						<td class="listeAdmis"><button class="embauche">Detail</button></td>
+					</tr>
+	
+				</table>
+              </div>
+            </div>
+        </div>
+
+</body>
+</html>
