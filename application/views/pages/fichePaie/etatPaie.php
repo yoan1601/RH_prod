856f56c98aefebd8e3a63809f2c63527ce238b83
JossElito179 <118587500+JossElito179@users.noreply.github.com>
2023-10-19 22:29:22 +0300

    fiche et etat de paie

---
+++ application/views/pages/fichePaie/etatPaie.php
@@ -0,0 +1,88 @@
+<?php 
+	$this->load->view('templates/header');
+	$this->load->view('templates/navbar');
+?>
+        
+        <center><h2>Etat de paie</h2></center>
+	  	<div class="card list">
+			<div class="card-body">
+				<p></p>
+				<p></p>
+				<p class="fin">nun</p>
+				<p><h3 class="card-title">Devise : ariary</h3></p>
+              <div class="list-group h">
+				<table>
+					<tr class="title_tex">
+						<td>Numero Matricule</td>
+                        <td>Sanitaire</td>
+                        <td>IRSA</td>
+                        <td>Total retenues</td>  
+                        <td>Salaire net</td>
+                        <td>Avance</td>
+                        <td>Net a payer</td>
+                        <td>Autres indemnites</td>
+                        <td>Net du mois</td> 
+                        <td class="listeAdmis">Actions</td>
+					</tr>
+
+					<tr class="vola">
+                        <td>001EMP</td>
+                        <td>1213</td>
+                        <td>4134</td>
+                        <td>3423</td>
+                        <td>4254534</td>
+                        <td>45345</td>
+                        <td>53453</td>
+                        <td>535</td>
+                        <td>3453</td>
+                        <td><center><button class="embauche">fiche de paie</button></center></td>
+                    </tr>
+
+                    
+					<tr class="vola">
+                        <td>001EMP</td>
+                        <td>1213</td>
+                        <td>4134</td>
+                        <td>3423</td>
+                        <td>4254534</td>
+                        <td>45345</td>
+                        <td>53453</td>
+                        <td>535</td>
+                        <td>3453</td>
+                        <td><center><button class="embauche">fiche de paie</button></center></td>
+                    </tr>
+
+                    
+					<tr class="vola">
+                        <td>001EMP</td>
+                        <td>1213</td>
+                        <td>4134</td>
+                        <td>3423</td>
+                        <td>4254534</td>
+                        <td>45345</td>
+                        <td>53453</td>
+                        <td>535</td>
+                        <td>3453</td>
+                        <td><center><button class="embauche">fiche de paie</button></center></td>
+                    </tr>
+
+                    
+					<tr class="vola">
+                        <td>001EMP</td>
+                        <td>1213</td>
+                        <td>4134</td>
+                        <td>3423</td>
+                        <td>4254534</td>
+                        <td>45345</td>
+                        <td>53453</td>
+                        <td>535</td>
+                        <td>3453</td>
+                        <td><center><button class="embauche">fiche de paie</button></center></td>
+                    </tr>
+				</table>
+              </div>
+            </div>
+        </div>
+
+</body>
+</html>
---
+++ application/views/pages/fichePaie/fichePaie.php
@@ -0,0 +1,10 @@
+<?php 
+	$this->load->view('templates/header');
+	$this->load->view('templates/navbar');
+?>
+        
+        <center><h2>Fiche de paie</h2></center>
+        <center><h2>Arrete au 31/08/2023</h2></center>
+
+</body>
+</html>
